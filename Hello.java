cbf61409d5dd76e30c7a6a895cb916b3cf039c87
Carlos Labigalini <carlos89.silva@catolicasc.org.br>
2020-08-19 22:00:01 -0300

    Primeiro programa java

---
+++ Hello.java
@@ -0,0 +1,12 @@
+public class Hello
+{
+	public static void main(String[] args)
+	{
+		System.out.print("Esta e uma saudacao: ");
+		System.out.println("hello, world!");
+		System.out.printf("Idade: 31");
+	}
+}
+
+// javac Hello.java - compila para bytecode (.class)
+// java Hello - executa o arquivo .class
